c5b21391bc080845ca603ff628c495e3490910b9
Darnol <32634736+Darnol@users.noreply.github.com>
2025-03-26 11:20:43 +0100

    WIP: Why does graph_manager and Checkov().run() output different local graphs?

--- explore_terraform/explore_terraform.py
+++ explore_terraform/explore_terraform.py
@@ -1,11 +1,24 @@
 import logging
 
+
+from checkov.main import Checkov
 from checkov.terraform.graph_manager import TerraformGraphManager
 
 # Set up logger
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+###
+# Checkov
+res = Checkov(
+    argv=[
+        "-d",
+        "./explore_terraform/terraform_examples/okd__guides__upi__vsphere_terraform",
+    ]
+).run()
+
+###
+# TerraformGraphManager
 graph_manager = TerraformGraphManager(
     db_connector=None,
 )
